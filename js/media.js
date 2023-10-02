7d82effb71679e961fa849d50c5e7e4f63339d05
Kirill-Web-Development <87230741+Kirill-Web-Development@users.noreply.github.com>
2023-10-02 22:20:10 +0300

    Add files via upload

--- js/media.js
+++ js/media.js
@@ -1,3 +1,14 @@
-function setMediaText(mediaWidth, elementSelector, mediaText) {
-    
+
+
+function setMediaText(mediaWidth, elementSelector, mediaText, initialText) {
+    const   windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    element = document.querySelector(elementSelector);
+    if (windowWidth <= mediaWidth) {
+        element.textContent = mediaText;
+    } else {
+        element.textContent = initialText;
+        console.log(11)
+    }
 }
+
+window.addEventListener('resize', () => setMediaText(768, '.upper-menu__callback-order-btn', 'Обратный звонок', 'Закать обратный звонок'));
--- js/setFullHeight.js
+++ js/setFullHeight.js
@@ -1,30 +1,20 @@
-function setFullHeight(selector) {
-    const elements = document.querySelectorAll(selector); // Выберите нужные элементы
-  
+function setFullHeight() {
+    const elements = document.querySelectorAll('.h-100'); // Выберите нужные элементы
+    console.log(elements)
     elements.forEach((element) => {
       const vh = window.innerHeight * 0.01; // 1% высоты окна браузера
       const offset = window.visualViewport.offsetTop; // Высота адресной строки браузера
       const height = (vh * 100) - offset; // Вычисляем высоту элемента
-  
       element.style.height = `${height}px`; // Устанавливаем высоту элемента
     });
-
-    const windowWidth = getWidthOfWindow();
-    const bottomMenu = document.querySelector('.bottom-menu')
-    if (windowWidth <=1000) {
-      bottomMenu.classList.add(selector)
-    } else {
-      bottomMenu.classList.remove(selector)
-  
-  }
   }
   
   // Вызываем функцию при загрузке страницы и при изменении размера окна
-  window.addEventListener('resize', () => setFullHeight('h-100'));
-  window.addEventListener('load', () => setFullHeight('h-100'));
+  window.addEventListener('resize', setFullHeight);
+  window.addEventListener('load', setFullHeight);
   
   // Применяем вычисленное значение в CSS
-  setFullHeight('h-100');
+  setFullHeight();
 
 function getWidthOfWindow() {
   const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
